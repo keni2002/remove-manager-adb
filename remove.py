40e01e702ad70f4023a780ad76b9302e505ed241
keni2002 <yerbisuniverses@gmail.com>
2021-06-24 00:07:11 -0400

    Comienza a tomar forma el código. Lo que hace ahora, informar si se conectó algun dispositivo en la PC y si tiene Depuración activada

--- remove.py
+++ remove.py
@@ -1,2 +1,15 @@
 #declarando librerías
 import os
+os.system("adb devices > .device.txt")
+f = open(".device.txt", "r")
+linea = f.readlines()[1]
+if len(linea) > 26:
+    print("\n **Por favor asegúrece de usar USB como Transferir Archivos**\n Saliendo...")
+    
+elif len(linea) > 7 and len(linea) < 26:
+    print("\nDispositivo conectado con éxito :)")
+    print("\n Dispositivo:\t" + linea )
+else:
+    print("\n**Asegúrece de conectar el teléfono a la PC y tener la depuración activa**\nSaliendo...")
+
+f.close()

413ab8b321badaedde7c8a4ea6104158eae95954
keni2002 <yerbisuniverses@gmail.com>
2021-07-04 16:22:41 -0400

    a new option of menu_of_task is added.  Changes for main loop, now we use a variable as a flag to stop it from a nested loop.  functionality is given to the first option of the task menu.  powerful uninstall error detection system. It was done when we're waiting for Tropical Storm Elsa in Cuba. :)

--- remove.py
+++ remove.py
@@ -6,6 +6,7 @@ import os
 #functions
 #Loadin Function
 lista = []
+
 #checker;
 checker = False
 def loadin(word):
@@ -67,7 +68,9 @@ elif len(linea) > 7 and len(linea) < 26:
     input("Press Enter to continue:")
         
     option = 0
-    while(True):
+    #to exit
+    exitir = False
+    while(exitir == False):
         
         os.system("clear")
         print(chr(27) +"[14;31m" + "<<REMOVE MANAGER ADB>>\n\tWelcome\t")
@@ -80,7 +83,9 @@ elif len(linea) > 7 and len(linea) < 26:
                                     ">> "))
             if option == 3:
                 # I close bucle
+                os.system("rm .check.dat")
                 break
+                
                 
                 os.system("clear")
             elif option == 1:
@@ -106,34 +111,102 @@ elif len(linea) > 7 and len(linea) < 26:
                
         if checker == True:
             
-            #try:
-                while(True):
+                #to exit
+                exitir2 = False
+                while(exitir2 == False):
                     os.system("clear")
-                    value_id  = int(input("Please insert the ID of desired package: "))
-                    
-
-                    if value_id > 0 and value_id <= len(lista):
-                        os.system("clear")
+                    try:
+                        value_id  = int(input("Please insert the ID of desired package: "))
                         
-                        print(chr(27) +"[14;31m" + "\t\t<<MENU OF TASKS>>\n\t" + chr(27) +"[14;35m"+"Package Selected: "+ lista[value_id-1]+"\t")
-                        option = int(input("\n" + chr(27) +"[0;30m" + "[Select an option to Manage the Application Pack]\n" +
-                                    " 1 Uninstall this Package\n" +
-                                    " 2 Uninstall but KEEP the data and cache directories of this Package\n" +
-                                    " 3 Exit\n" +
-                                    ">> "))
-                    
-                    
-                        #restart list
-                        lista = []
-                    
-                    
-                    else:
-                      z =  input(chr(27) +"[14;31m"+ "FAILURE: ID out of package range.\n" +
-                                chr(27) +"[0;30m" +
-                                "Range=[1-"+ str(len(lista))+"]")
 
-            #except:
-                #pass    
+                        if value_id > 0 and value_id <= len(lista):
+                            while(True):
+                                os.system("clear")
+                                try:
+                                    print(chr(27) +"[14;31m" + "\t\t<<MENU OF TASKS>>\n\t" + chr(27) +"[14;35m"+"Package Selected: "+ lista[value_id-1]+"\t\n\nPROCEED WITH CARE!!\n")
+                                    option = int(input("\n" + chr(27) +"[0;30m" + "[Select an option to Manage the Application Pack]\n" +
+                                                " 1 Uninstall this Package\n" +
+                                                " 2 Uninstall but KEEP the data and cache directories of this Package\n" +
+                                                " 3 Return to Load Packages\n" +
+                                                " 4 Exit\n" +
+                                                ">> "))
+                                    if option == 4:
+                                        #EXIT
+                                        exitir2 = True
+                                        exitir = True
+                                        os.system("rm .check.dat")
+                                        break
+                                        
+                                    elif option == 1:
+                                        #UNINSTALL
+                                        os.system("clear")
+                                        print(chr(27) +"[1;31m"+"YOU'RE ABOUT TO PERMANENTLY UNINSTALL THE PACKAGE: \n" +
+                                                        chr(27) +"[1;34m"+
+                                                        lista[value_id-1]+chr(27) +"[0;30m")
+                                        z = str(input("Do you want to uninstall this package? (yes/N): "))
+                                        if z == 'yes':
+                                            #It is important this slicing because there is a hidden \n to the end of the list.
+                                            outputs = os.system("adb uninstall " + lista[value_id-1][0:-1]+ "> .check.dat")
+                                            flow_output = open(".check.dat", "r")
+                                            if_error = flow_output.readline()
+                                            flow_output.close()
+                                            os.system("clear")
+                                            if if_error[0:-1] == "Success":
+                                                input(chr(27) +"[1;32m" + "The package: " + 
+                                                                            lista[value_id-1] +
+
+                                                                            " was uninstalled successfully :-)" +
+                                                                            chr(27) +"[1;34m"+
+                                                                            "\nNothing more to do so"+
+                                                                            chr(27) +"[0;30m"+
+                                                                            "\nPress Enter to return to MAIN MENU >> ")
+                                                lista = []
+                                                exitir2 = True
+                                                break
+                                            elif if_error[0:-1] == "Failure [DELETE_FAILED_INTERNAL_ERROR]":
+                                                errore = input(chr(27) +"[1;31m" + "INTERNAL ERROR, Maybe " 
+                                                            + chr(27) +"[1;34m" + 
+                                                            lista[value_id-1][0:-1] +
+                                                            chr(27) +"[1;31m\n" + 
+                                                            " is a system package and your phone is not Rooted"+
+                                                            chr(27) +"[0;30m" +
+                                                            "\nPress Enter to return >> ")
+                                            else:
+                                                input("UNKNOWN FAILURE"+ if_error)
+                                            #restart list and return to load
+                                            
+                                            
+                                        else:
+                                            input("<<ABORTED>>\nPress Enter to Return to Menu of Task >> ")
+
+                                    elif option == 2:
+                                        #SAVE DATA
+                                        pass
+                                    elif option == 3:
+                                        #Select package again
+                                        #restart list and exit from task menu
+                                        lista = []
+                                        exitir2 = True
+                                        break
+
+                                    else:
+                                        z =  input(chr(27) +"[14;31m"+ "FAILURE!! NO OPTION: "+str(option) +
+                                                chr(27) +"[0;30m")
+                                except ValueError:
+                                    input(chr(27) +"[14;31m"+"FAILURE! INSERT ONLY INTEGER NUMBER"+ chr(27) +"[0;30m" )
+
+
+                        
+                            
+                        
+                        
+                        else:
+                            z =  input(chr(27) +"[14;31m"+ "FAILURE: ID out of package range.\n" +
+                                        chr(27) +"[0;30m" +
+                                        "Range=[1-"+ str(len(lista))+"]")
+
+                    except ValueError:
+                        input(chr(27) +"[14;31m"+"FAILURE! INSERT ONLY INTEGER NUMBER"+ chr(27) +"[0;30m" ) 
            # while(True):
 
             #break # maybe this line was delete in the future
